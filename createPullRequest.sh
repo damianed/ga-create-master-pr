fc33d674e9435eee5122a9dc723d755a1f134499
Damian Escobedo <damian.escobedo@justia.com>
2020-09-23 14:11:34 -0500

    add body

--- createPullRequest.sh
+++ createPullRequest.sh
@@ -2,12 +2,11 @@
 
 currentDate=$(date +'%B %d, %Y')
 
-body=$(jq -r . "$GITHUB_EVENT_PATH")
-echo ${currentDate}
+body=$(jq -r after.commits.0.message "$GITHUB_EVENT_PATH")
 echo ${GITHUB_REPOSITORY} 
 curl -sSL \
   -X POST \
   -H "Authorization: token ${GITHUB_TOKEN}" \
   -H "Accept: application/vnd.github.v3+json" \
-  -d "{\"title\":\"Live Push: ${currentDate}\",\"head\":\"develop\",\"base\":\"master\", \"body\": \"body\"}" \
+  -d "{\"title\":\"Live Push: ${currentDate}\",\"head\":\"develop\",\"base\":\"master\", \"body\": \"Develop PR: ${body}\"}" \
   "https://api.github.com/repos/${GITHUB_REPOSITORY}/pulls"
